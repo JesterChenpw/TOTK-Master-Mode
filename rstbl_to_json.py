c29345ed3a4dcbfa75016e84083401adaa0cecf1
Echocolat <98425838+Echocolat@users.noreply.github.com>
2023-09-08 17:05:20 +0200

    restbl values ; start of golden moblin

--- rstbl_to_json.py
+++ rstbl_to_json.py
@@ -1,9 +1,16 @@
 import json
 
-with open('rstbel.txt') as rstb_text:
+with open('restbl.txt') as rstb_text:
     rstb_txt = rstb_text.readlines()
-    for text in range(len(rstb_txt)):
-        rstb_txt[text] = rstb_txt[text][:-1]
+
+rstb_dict = {}
+rstb_txt = sorted(rstb_txt)
+
+for line in rstb_txt:
+    if not line == '\n':
+        line = line[:-1]
+        entry, size = line.split(' : ')
+        rstb_dict[entry] = int(size)
 
 with open('restbl.json', 'w') as json_file:
-    json_file.write(json.dumps(sorted(rstb_txt), indent = 2))
+    json_file.write(json.dumps(rstb_dict, indent = 2))
