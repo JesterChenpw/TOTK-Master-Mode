716bba42c0b8c0e7676206bf5b0e2be8851eb68a
Echocolat <98425838+Echocolat@users.noreply.github.com>
2023-12-31 11:46:13 +0100

    moblin horn :D

---
+++ hashes.py
@@ -0,0 +1,25 @@
+import mmh3
+
+# Whatever you want here
+actor_list = ["Item_Enemy_902", "Item_Enemy_902_R"]
+
+def hash(value):
+    hash = hex(mmh3.hash(value, signed=False))
+    while len(hash) != 10:
+        hash = hash[:2] + '0' + hash[2:] # add 0's to pad to the right length, just remove if unwanted
+    return hash
+
+# Just bump the value after the < if you want it to be further to the right
+def format(actor, prefix='', suffix=''):
+    return f"{prefix + actor + suffix:<45}{': ' + hash(prefix + actor + suffix)}"
+
+with open('hashes.txt', 'w') as file:
+    for actor in actor_list:
+        print(format(actor), file=file)
+        print(format(actor, 'PictureBookData.'), file=file)
+        print(format(actor, 'PictureBookData.', '.IsNew'), file=file)
+        print(format(actor, 'PictureBookData.', '.State'), file=file)
+        if "Item_" in actor or "Weapon_" in actor:
+            print(format(actor, 'IsGet.'), file=file)
+            print(format(actor, 'IsGetAnyway.'), file=file)
+        print('', file=file) # add a newline to separate individual actors
