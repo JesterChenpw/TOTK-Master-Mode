6cdaa50254f4332df8da53fed89f135759dbf241
Echocolat <98425838+Echocolat@users.noreply.github.com>
2024-04-28 12:03:00 +0200

    cleaning

--- generate_restbl.py
+++
@@ -1,9 +0,0 @@
-from RESTBL.src.restbl import MergeMods
-import os
-
-def generate_restbl(version):
-
-    MergeMods('Mods/' + str(version), 'C://Users//lezen//Desktop//TotK Dump//' + str(version) + '//romfs', version = version, smart_analysis = False, checksum = True)
-    os.remove(f'ResourceSizeTable.Product.{version}.rsizetable')
-
-generate_restbl(110)
